f2370fbc86bb8c5b6a1f6711142a040aaefe09c7
konan-5 <ikedahiroshi517@gmail.com>
2024-01-25 08:01:29 +0000

    update search bar

--- components/Header/SeachBar.tsx
+++ components/Header/SeachBar.tsx
@@ -1,24 +1,46 @@
 "use client"
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { categories } from "./categoryData";
 
 const SearchBar = () => {
     const [keyword, setKeyword] = useState('');
+    const [filteredCategories, setFilteredCategories] = useState([]);
+    const sortedCategory = categories.slice().sort((a, b) => { return a.substring(9).localeCompare(b.substring(9)) });
+    useEffect(() => {
+        if (keyword) {
+            const filteredCategories = sortedCategory.filter(category =>
+                category.toLowerCase().includes(keyword.toLowerCase())
+            );
+            setFilteredCategories(filteredCategories.slice(0, 10));
+        }
+    }, [keyword])
     return (
         <div className="relative z-10">
-            <div className="flex justify-center items-center relative overflow-hidden p-[10px] bg-white rounded-md">
+            <div className="flex justify-center items-center relative overflow-hidden p-[10px] bg-gray-light dark:bg-dark rounded-md">
                 <input
-                    className="w-full lg:w-[500px] px-4 text-lg font-bold text-navy bg-white focus:outline-0"
+                    className="w-full lg:w-[500px] px-4 text-lg font-bold text-body-color dark:text-body-color-dark bg-gray-light dark:bg-dark focus:outline-0"
                     placeholder="Search Tenders..."
                     onChange={(e) => setKeyword(e.target.value)}
+                    onBlur={() => setKeyword('')}
+                    value={keyword}
                 />
-                <div className="w-[35px] h-[35px] flex justify-center items-center bg-[#4382CA] hover:bg-[#3069aa] rounded-md">
-                    <i className='bx bx-search text-xl'></i>
+                <div className="absolute right-[10px] w-[35px] h-[35px] flex justify-center items-center bg-[#4382CA] rounded-md">
+                    <i className={`bx search text-xl text-white bx-${keyword ? 'x' : 'search'}`}></i>
                 </div>
             </div>
             {
-                keyword &&
-                <div className="absolute w-full bg-white rounded-md h-[200px] border-t-[1px] ">
 
+                keyword &&
+                <div className="absolute w-full bg-gray-light dark:bg-dark rounded-md">
+                    {
+                        filteredCategories.map((item, key) => {
+                            return (
+                                <button className="px-4 py-1 border-t-[1px] dark:border-body-color hover:bg-white dark:hover:bg-body-color text-body-color dark:text-white whitespace-nowrap overflow-hidden text-ellipsis w-full text-left">
+                                    {item.substring(9)}
+                                </button>
+                            )
+                        })
+                    }
                 </div>
             }
         </div>
